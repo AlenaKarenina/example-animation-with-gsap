4e64e0ae7cad776ea840d150462af6560576855b
Alena Karenina <karena1q84@gmail.com>
2024-02-22 19:01:04 +0500

    feat: add animation gsap for content

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,117 @@
-import Image from "next/image";
+'use client'
+import { useEffect } from 'react'
+import Image from 'next/image'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
+
+  useEffect(() => {
+    const texts = document.querySelector('.texts') as HTMLDivElement
+
+    gsap.to('.images', {
+      scrollTrigger: {
+        trigger: '.scrolled',
+        //pin: '.images',
+        scrub: 0.5,
+        start: 'top top',
+        end: () => '+=' + (texts.scrollHeight - window.innerHeight),
+        id: 'scrolled'
+      }
+    })
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.scrolled',
+        start: 'top top',
+        end: 'bottom',
+        pin: false,
+        scrub: true,
+        id: 'img'
+      }
+    })
+
+    const imagesBlock = gsap.utils.toArray('.image-block') as string[]
+
+    imagesBlock.forEach((image: string, i) => {
+      if (imagesBlock[i + 1]) {
+        tl.to(image, { opacity: 0, zIndex: -1 }, '+=0.2').to(
+          imagesBlock[i + 1],
+          { opacity: 1, zIndex: 0 },
+          '<'
+        );
+      }
+    })
+
+    tl.to({}, {}, '+=0.2')
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="wrapper">
+
+      <div className="scrolled">
+        <div className="images">
+          <div className="first-image image-block">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/images/1.jpg"
+              alt="unsplash image"
+              priority={true}
+              width={640}
+              height={960}
             />
-          </a>
+          </div>
+
+          <div className="second-image image-block">
+            <Image
+              src="/images/2.jpg"
+              alt="unsplash image"
+              priority={true}
+              width={640}
+              height={960}
+            />
+          </div>
+
+          <div className="third-image image-block">
+            <Image
+              src="/images/3.jpg"
+              alt="unsplash image"
+              priority={true}
+              width={640}
+              height={960}
+            />
+          </div>
+
+          <div className="fourth-image image-block">
+            <Image
+              src="/images/4.jpg"
+              alt="unsplash image"
+              priority={true}
+              width={640}
+              height={960}
+            />
+          </div>
+        </div>
+
+        <div className="texts">
+          <div className="first-text text-block">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at est eget metus convallis tempus. Quisque sit amet placerat nisl.<br/>Proin luctus velit nec nibh rutrum consequat. Suspendisse id mauris lorem.<br/>Donec tincidunt vulputate nulla vel lacinia. Etiam sollicitudin ante eget lectus luctus, at consequat nulla commodo. Morbi luctus diam non lorem blandit luctus.<br/>Mauris consequat imperdiet felis, id mattis nulla facilisis vel. Cras sed tempor lacus, maximus posuere risus. Sed erat enim, dictum eu dui ac, dignissim scelerisque arcu.</p>
+          </div>
+
+          <div className="second-text text-block">
+            <p>Lorem ipsum dolor sit amet, consectetur <br/>adipiscing elit. Integer at est eget metus convallis tempus. Quisque sit amet placerat nisl. Proin luctus velit nec nibh rutrum consequat. Suspendisse id mauris lorem. <br/>Donec tincidunt vulputate nulla vel lacinia. Etiam sollicitudin ante eget lectus luctus, at consequat<br/> nulla commodo. Morbi luctus diam non lorem blandit luctus. Mauris consequat<br/> imperdiet felis, id mattis nulla facilisis vel. Cras sed tempor lacus, maximus posuere risus. Sed erat enim,<br/> dictum eu dui ac, dignissim scelerisque arcu.</p>
+          </div>
+
+          <div className="third-text text-block">
+            <p>Lorem ipsum dolor sit amet consectetur <br/>adipisicing elit. <br/>Excepturi maxime placeat, <br/>perspiciatis dolorem tempora sint id <br/>iure in odio blanditiis vitae recusandae, <br/>eligendi dolore fugit provident, <br/>amet minus aperiam ad. <br/>Quibusdam hic quam accusamus, <br/>architecto et laboriosam dolor mollitia placeat, <br/>molestiae qui natus, <br/>incidunt aliquam laudantium dolore! <br/>Ipsam totam harum obcaecati repellat ducimus commodi. <br/>Delectus repellendus, <br/>ea quia velit in est architecto nobis dolorem ullam? <br/>Voluptatem laboriosam, <br/>sapiente id vero numquam commodi dolores vitae? <br/>Facilis, <br/>rem alias fugit eligendi optio facere maiores laborum in cumque laboriosam similique, <br/>eum at sequi unde fugiat vero quia iure ipsa quod, <br/>dolor labore asperiores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur <br/>adipisicing elit. <br/>Excepturi maxime placeat, <br/>perspiciatis dolorem tempora sint id <br/>iure in odio blanditiis vitae recusandae, <br/>eligendi dolore fugit provident, <br/>amet minus aperiam ad. <br/>Quibusdam hic quam accusamus, <br/>architecto et laboriosam dolor mollitia placeat, <br/>molestiae qui natus, <br/>incidunt aliquam laudantium dolore! <br/>Ipsam totam harum obcaecati repellat ducimus commodi. <br/>Delectus repellendus, <br/>ea quia velit in est architecto nobis dolorem ullam? <br/>Voluptatem laboriosam, <br/>sapiente id vero numquam commodi dolores vitae? <br/>Facilis, <br/>rem alias fugit eligendi optio facere maiores laborum in cumque laboriosam similique, <br/>eum at sequi unde fugiat vero quia iure ipsa quod, <br/>dolor labore asperiores.</p>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="other">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis odio sequi quaerat nisi a rem quis impedit, et consequuntur reprehenderit quidem dolorem, quas perspiciatis ab quam aperiam quia facere. Similique!</p>
       </div>
     </main>
-  );
+  )
 }
